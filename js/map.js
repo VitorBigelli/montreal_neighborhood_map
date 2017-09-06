f37649132a927a84ac5d80bd95b3ea850f2c71e2
Vitor <vitorbigelli@hotmail.com>
2017-09-05 23:05:15 -0300

    Add hamburger icon.

--- js/map.js
+++ js/map.js
@@ -1,27 +1,31 @@
 // ------- MODEL ------ //
-var model = function() {
+var model = {
+	map: null,
+	markers: [],
+	polygon: null,
+	infowindow: null
 };
 
-var map; 
-var markers; 
-var polygon;
-var infowindow;
-
-
-
+var locations = [];
 
 var ViewModel = function() {
 
 
+
 	function initMap() {
 		var center = {lat: 37.3875, lng: -122.0575};
-		map = new google.maps.Map(document.getElementById("map"), {
+		model.map = new google.maps.Map(document.getElementById("map"), {
 			center: center, 
 			zoom: 13
 		});
 	}
-
 	initMap();
+
+	document.getElementById("hamburger-menu").addEventListener("click", function(e) {
+		$(document.getElementById("options-box")).toggleClass("hide");
+		$(document.getElementById("map")).toggleClass("translate");
+		$(document.getElementById("header")).toggleClass("translate");
+	});
 
 }; 
 
