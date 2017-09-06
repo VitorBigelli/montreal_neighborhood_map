caa0d196cfb591edae26943680d3735f8d251a1c
Vitor <vitorbigelli@hotmail.com>
2017-09-06 08:12:25 -0300

    Create array of locations.

--- js/map.js
+++ js/map.js
@@ -6,7 +6,16 @@ var model = {
 	infowindow: null
 };
 
-var locations = [];
+var locations = [
+	{title: "The Tech Museum of Innovation", location: {lat: 37.3316, lng: -121.8901}},
+	{title: "Computer History Museum", location: {lat: 37.4143, lng: -122.0774}}, 
+	{title: "Happy Hollow Park & Zoo", location: {lat: 37.3257, lng: -121.8614}}, 
+	{title: "Children's Discovery Museum of San Jose", location: {lat: 37.3268, lng: -121.8925}}, 
+	{title: "Googleplex", location: {lat: 37.4220, lng: -122.0841}}, 
+	{title: "Facebook HQ", location: {lat: 37.4847, lng: -122.1477}}, 
+	{title: "Tesla, Inc.", location: {lat: 37.394705, lng: --122.150325}}, 
+	{title: "Nvidia", location: {lat: 37.370728, lng: -121.963739}}
+];
 
 var ViewModel = function() {
 
