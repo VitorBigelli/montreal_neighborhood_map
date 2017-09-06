1336ac6d6b2d932ea35508764423a0dd01a4b92c
Vitor <vitorbigelli@hotmail.com>
2017-09-05 22:05:35 -0300

    Basic Style.

--- js/map.js
+++ js/map.js
@@ -1,20 +1,27 @@
 // ------- MODEL ------ //
+var model = function() {
+};
+
 var map; 
 var markers; 
 var polygon;
 var infowindow;
 
-function initMap() {
-	var center = {lat: 37.3875, lng: -122.0575};
-	map = new google.maps.Map(document.getElementById("map"), {
-		center: center, 
-		zoom: 13
-	});
-}
+
 
 
 var ViewModel = function() {
 
+
+	function initMap() {
+		var center = {lat: 37.3875, lng: -122.0575};
+		map = new google.maps.Map(document.getElementById("map"), {
+			center: center, 
+			zoom: 13
+		});
+	}
+
+	initMap();
+
 }; 
 
-ko.applyBindings(ViewModel);
