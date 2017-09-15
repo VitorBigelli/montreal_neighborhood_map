f5c86f1db3885adfaec883f92f765a807d549819
Vitor <vitorbigelli@hotmail.com>
2017-09-15 14:04:07 -0300

    Move the locations array to a different file.

---
+++ js/data.js
@@ -0,0 +1,11 @@
+// Array of locations to be displayed
+var locations = [
+	{title: "The Tech Museum of Innovation", position: {lat: 37.3316, lng: -121.8901}},
+	{title: "Computer History Museum", position: {lat: 37.4143, lng: -122.0774}}, 
+	{title: "Happy Hollow Park & Zoo", position: {lat: 37.3257, lng: -121.8614}}, 
+	{title: "Children's Discovery Museum of San Jose", position: {lat: 37.3268, lng: -121.8925}}, 
+	{title: "Googleplex", position: {lat: 37.4220, lng: -122.0841}}, 
+	{title: "Facebook HQ", position: {lat: 37.4847, lng: -122.1477}}, 
+	{title: "Tesla, Inc.", position: {lat: 37.394705, lng: -122.150325}}, 
+	{title: "Nvidia", position: {lat: 37.370728, lng: -121.963739}}
+];
